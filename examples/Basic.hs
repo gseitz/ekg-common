5f7aae29ffcaf1d968b0cc4fb27f3de7255b4c61
Gerolf Seitz <gerolf.seitz@gmail.com>
2013-01-10 02:18:55 +0100

    Add examples for Meter and Histogram usage.

--- examples/Basic.hs
+++ examples/Basic.hs
@@ -6,17 +6,45 @@ module Main where
 
 import Control.Concurrent
 import Control.Exception
+import Control.Monad
 import qualified System.Remote.Counter as Counter
 import qualified System.Remote.Label as Label
 import System.Remote.Ekg
 import Data.Aeson
 import System.Remote.Stats.Histogram
+import System.Remote.Stats.Meter
+import System.Remote.Stats.Time
+
+import System.Posix.Unistd
 
 mean :: Fractional a => [a] -> a
 mean xs = sum xs / fromIntegral (length xs)
 
-main :: IO ()
-main = do
+-- | This function demonstrates Meter
+-- The meter is marked with 30 events initially. After that, one thread records
+-- 5 events every second, whereas another thread prints the number of marked
+-- events and the current 1/5/15 minute rates every 2 seconds.
+-- Watch the rates go down from 11 to 5 over time.
+runMeter :: IO ()
+runMeter = do
+    meter <- newMeter Second
+    mark meter 30
+    forkIO $ forever $ do
+        c <- count meter
+        m1 <- oneMinuteRate meter
+        m5 <- fiveMinuteRate meter
+        m15 <- fifteenMinuteRate meter
+        putStrLn $ show $ (c, m1, m5, m15)
+        sleep 2
+
+    forkIO $ forever $ do
+        mark meter 5
+        sleep 1
+
+    return ()
+
+runHistogram :: IO ()
+runHistogram = do
     h <- newUniformHistogram
     update h 3
     update h 5
@@ -24,6 +52,11 @@ main = do
     mmin <- hMin h
     mmax <- hMax h
     putStrLn $ show (mmin, mmax, mean)
+
+main :: IO ()
+main = do
+    runMeter
+
     --reg <- newRegistry
     --initializeBuiltInStats reg
     --counter <- getCounter "iterations" reg
